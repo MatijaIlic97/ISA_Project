331e543f3bf4f5c4b35cbe0ec5edf3b0661067f8
IlicMatija <lolyepreferral@gmail.com>
2024-06-08 00:11:47 +0200

    Initial Project

---
+++ src/main/java/com/proba/artifact/controllers/UserController.java
@@ -0,0 +1,30 @@
+package com.proba.artifact.controllers;
+
+import com.proba.artifact.models.UserModel;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("user")
+public class UserController {
+    @GetMapping("getfirstname")
+    public String getFirstName(){
+        return "nesto";
+    }
+
+    @GetMapping("getlist")
+    public List<String> getList(){
+        return List.of("nesto3", "nesto2");
+    }
+
+    @PostMapping("createuser")
+    public boolean createUser(String firstName, String lastName){
+        return true;
+    }
+
+    @PostMapping("createuserbody")
+    public UserModel createUserBody(@RequestBody UserModel userModel){
+        return userModel;
+    }
+}
