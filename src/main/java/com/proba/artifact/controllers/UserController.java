4fe1e52f604e8b1ddfd6d09f53fed9142d25593f
IlicMatija <lolyepreferral@gmail.com>
2024-06-25 02:17:02 +0200

    Initial Project

--- src/main/java/com/proba/artifact/controllers/UserController.java
+++ src/main/java/com/proba/artifact/controllers/UserController.java
@@ -10,7 +10,7 @@ import java.util.List;
 public class UserController {
     @GetMapping("getfirstname")
     public String getFirstName(){
-        return "nesto";
+        return "nesto123";
     }
 
 
